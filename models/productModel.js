b36ffafdecb45a7ba1f4a69ae457d05926c6cf67
enirevescode <enirevescode@gmail.com>
2023-12-14 12:36:28 +0100

    ajout d'item ds productmodel

--- models/productModel.js
+++ models/productModel.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose')
 // Modèle user
 const ProductSchema = mongoose.Schema({
     productName: {type: String, require: 'Le nom du produit est obligatoire', unique: true},
+    alt: {type: String, require: true},
     category: {type: String, require: true},
-    price: {type: String, require: true},
+    color: {type: String, require: true},
+    price: {type: Number, require: true},
     description: {type: String, require: true},
     composition: {type: String, require: true},
     photos: {type: String, require: true},
-    avis: {type: String, default: ''}
+    avis: {type: String, default: ''},
+    avis2: {type: String, default: ''}
 })
 
 ProductSchema.pre('save', async function() {
