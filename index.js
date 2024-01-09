42493949bbd831afa3ccf4a68bc90a2d8f992aa1
enirevescode <enirevescode@gmail.com>
2024-01-09 16:09:41 +0100

    modif

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const user_router = require('./routes/usersRoutes')
 //const produit_router = require('./routes/produits')
 
 /*****MISE EN PLACE DU ROUTAGE */
-app.get('/', (req, res) => res.send(`Serveur node et express sur port ${PORT}`))
+app.get('/', (req, res) => res.send(`Serveur node et express sur port ${SERVER_PORT}`))
 app.use('/users', user_router)
 
 app.get('*', (req, res) => res.status(501).send(`hey ! La ressource n'existe pas !`))
@@ -20,8 +20,8 @@ mongoose
     .connect(process.env.MONGO_DB_CONNECTIONSTRING)
     .then(() =>{
         console.log("Database mongodb is connected!");
-        app.listen(process.env.PORT, () => {
-            console.log(`This server is running on port ${process.env.PORT}. Connected !`)
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`This server is running on port ${process.env.SERVER_PORT}. Connected !`)
         })
 })
 .catch(error => console.log('Connection Database Error', error))
